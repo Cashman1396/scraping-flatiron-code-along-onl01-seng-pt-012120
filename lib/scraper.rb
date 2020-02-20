5d222b643b2aa68438af23ab04c991a4b1203214
Chris Ashman <chrisashman96@gmail.com>
2020-02-20 00:28:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,10 +9,7 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     
-      #course = Course.new
-      #course.title = post.css("h2").text
-      #course.schedule = post.css(".date").text
-      #course.description = post.css("p").text
+    
   end 
   
   def get_courses
@@ -26,6 +23,7 @@ class Scraper
     course.schedule = post.css(".date").text
     course.description = post.css("p").text
   end
+end
   
   
   def print_courses
@@ -38,7 +36,6 @@ class Scraper
       end
     end
   end
-  
 end
 
 
